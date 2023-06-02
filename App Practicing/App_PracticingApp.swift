475a282ffbebd653101e7e7ddca13dd106c2ffeb
CoyleWasHere <64401148+CoyleWasHere@users.noreply.github.com>
2023-06-01 20:42:52 -0400

    Initial Commit

---
+++ App Practicing/App_PracticingApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_PracticingApp.swift
+//  App Practicing
+//
+//  Created by Daniel Coyle on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct App_PracticingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
