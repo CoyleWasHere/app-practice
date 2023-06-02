4cb6f473fae1c7865b4f7c303cd78864de8ea372
CoyleWasHere <64401148+CoyleWasHere@users.noreply.github.com>
2023-06-01 20:52:58 -0400

    - Practice Session

--- App Practicing/ContentView.swift
+++ App Practicing/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("This Is My Practice Session")
         }
         .padding()
     }
